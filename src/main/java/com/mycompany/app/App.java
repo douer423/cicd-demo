21a3bcbf24701876d25cedac64d5d1c1666fe9e4
douer423 <cfc23@sina.cn>
2020-09-04 18:22:11 +0800

    webhook Test

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello CICD World!webhook test~" );
+        System.out.println( "Hello CICD World!webhook test" );
     }
 }
